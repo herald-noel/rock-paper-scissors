e4d71018efa19b1416f02fa623fc39743718b59f
herald-noel <bontilao.heraldnoel@gmail.com>
2022-08-22 06:58:36 +0800

    Add a round and a scoring system

--- script.js
+++ script.js
@@ -15,11 +15,36 @@ function playerSelection() {
 
 function playRound(playerSelection, computerSelection) {
     let flag = 0;
-    if (playerSelection === computerSelection) return 'It\'s a tie!';
-    else if (playerSelection === 'paper' && computerSelection === 'rock') flag = 1;
-    else if (playerSelection === 'rock' && computerSelection === 'scissors') flag = 1;
-    else if (playerSelection === 'scissors' && computerSelection === 'paper') flag = 1;
-    
-    if (flag) return `You win! ${playerSelection} beats ${computerSelection}.`;
-    else `You lose! ${computerSelection} beats ${playerSelection}.`;
+    if (playerSelection === computerSelection) return 2;
+    else if (playerSelection === 'paper' && computerSelection === 'rock') return 1;
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') return 1;
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') return 1;
 }
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (true) {
+        const playerChoice = playerSelection();
+        const computerChoice = getComputerChoice();
+
+        let winner = playRound(playerChoice, computerChoice);
+
+        if (winner == 1) {
+            playerScore++;
+            alert(`You win! ${playerChoice} beats ${computerChoice}.   Score is ${playerScore} - ${computerScore}`);
+        }
+        else if (winner == 2) {
+            alert(`It's a tie!   Score is ${playerScore} - ${computerScore}`);
+        }
+        else {
+            computerScore++;
+            alert(`You lose! ${computerChoice} beats ${playerChoice}.   Score is ${playerScore} - ${computerScore}`);
+        }
+
+        if (playerScore === 5 || computerScore === 5) break;
+    }
+}
+
+game();
