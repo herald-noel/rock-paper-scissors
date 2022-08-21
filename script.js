dd3e180a2af788a4f46cbd8d67ab5ed182d23135
herald-noel <bontilao.heraldnoel@gmail.com>
2022-08-22 06:21:55 +0800

    Add a round function

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function playerSelection() {
     let answer = prompt("Rock, Paper, Scissors");
     return answer.toLowerCase();
 }
+
+function playRound(playerSelection, computerSelection) {
+    let flag = 0;
+    if (playerSelection === computerSelection) return 'It\'s a tie!';
+    else if (playerSelection === 'paper' && computerSelection === 'rock') flag = 1;
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') flag = 1;
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') flag = 1;
+    
+    if (flag) return `You win! ${playerSelection} beats ${computerSelection}.`;
+    else `You lose! ${computerSelection} beats ${playerSelection}.`;
+}
